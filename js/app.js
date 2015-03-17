2eb3ba2dc19da117191d4edff01df19eec88a11a
Roberto Salgado <rsalgado03@gmail.com>
2015-03-17 23:46:27 +0000

    Use JSON data for bio section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module("resumeApp", []);
+(function() {
+  var app = angular.module("resumeApp", []);
+
+  // Main Controller
+  app.controller("ResumeController", ["$http", function($http) {
+    this.resume = {};
+
+    // HTTP request for retrieving JSON with Resume contents
+    var self = this;
+    $http.get("/js/resume.json")
+      .success(function(data) {
+        self.resume = data;
+      })
+      .error(function(error) {
+        console.log("Error retriving JSON in ResumeController: " + error);
+      });
+  }]);
+})();
